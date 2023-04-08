ed6b5ba342a52e101918d099cba8d06f02c306e7
Richard Leonardo <85978263+Rikat19@users.noreply.github.com>
2023-04-08 21:56:45 +0700

    DatabaseHandler

---
+++ app/src/main/java/com/example/ppb_comffee/DatabaseHandler.kt
@@ -0,0 +1,26 @@
+package com.example.ppb_comffee
+
+import android.os.StrictMode
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+
+class DatabaseHandler {
+    var con: Connection? = null
+    var url = "jdbc:mysql://10.0.2.2/bookacafe"
+    var username = "root"
+    var password = ""
+
+    fun connect(): Connection? {
+        try {
+            val policy = StrictMode.ThreadPolicy.Builder()
+                .permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+            con = DriverManager.getConnection(url, username, password)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        }
+        return con
+    }
+}
+}
