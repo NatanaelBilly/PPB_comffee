a4805087785888c36ffaf2a049a47e9a56d3e345
Richard Leonardo <85978263+Rikat19@users.noreply.github.com>
2023-04-12 14:26:29 +0700

    Fix Login

--- app/src/main/java/com/example/ppb_comffee/LoginFragment.kt
+++ app/src/main/java/com/example/ppb_comffee/LoginFragment.kt
@@ -11,16 +11,33 @@ import androidx.fragment.app.FragmentManager
 
 class LoginFragment : Fragment(), View.OnClickListener {
 
-    companion object {
-        const val EMAIL = "email"
-        const val PASS = "pass"
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_login -> {
+                val mFragmentManager = fragmentManager as FragmentManager
+                val mHomePage = HomePage()
+                mFragmentManager
+                    .beginTransaction()
+                    .replace(
+                        R.id.frame_container,
+                        mHomePage,
+                        LoginFragment::class.java.simpleName
+                    )
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
     }
 }
